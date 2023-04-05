8f8fce47337d2d8f895fe1639643ecfa77dc8b8d
upinetree <upinetree@gmail.com>
2023-04-05 16:30:41 +0000

    Support request verification with bearer token for development

--- go.mod
+++ go.mod
@@ -5,4 +5,7 @@ go 1.20
 require (
 	github.com/aws/aws-lambda-go v1.39.1
 	github.com/sashabaranov/go-openai v1.5.6
+	github.com/slack-go/slack v0.12.1
 )
+
+require github.com/gorilla/websocket v1.4.2 // indirect
--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"net/http"
 	"os"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/sashabaranov/go-openai"
+	"github.com/slack-go/slack"
 )
 
 type ChatRequest struct {
@@ -21,11 +25,17 @@ type ChatResponse struct {
 
 func HandleRequest(ctx context.Context, request events.LambdaFunctionURLRequest) (events.LambdaFunctionURLResponse, error) {
 	fmt.Printf("Processing request data for request %s.\n", request.RequestContext.RequestID)
+	fmt.Printf("Request headers: %+v.\n", request.Headers)
 	fmt.Printf("Request body: %s.\n", request.Body)
+
+	err := config.requestVerifier.Verify(request)
+	if err != nil {
+		return events.LambdaFunctionURLResponse{Body: err.Error(), StatusCode: http.StatusUnauthorized}, nil
+	}
 
 	var chatRequest ChatRequest
 
-	err := json.Unmarshal([]byte(request.Body), &chatRequest)
+	err = json.Unmarshal([]byte(request.Body), &chatRequest)
 	if err != nil {
 		return events.LambdaFunctionURLResponse{Body: err.Error(), StatusCode: 400}, nil
 	}
@@ -65,10 +75,94 @@ func sendToOpenAI(ctx context.Context, message string) (string, error) {
 	return resp.Choices[0].Message.Content, nil
 }
 
+type requestVerifier interface {
+	Verify(events.LambdaFunctionURLRequest) error
+}
+
+type bearerVerifier struct {
+	secret string
+}
+
+type slackSignedSecretVerifier struct {
+	secret string
+}
+
+// Verify the request with slack secret verification
+func (v slackSignedSecretVerifier) Verify(r events.LambdaFunctionURLRequest) error {
+	header := http.Header{}
+	for k, v := range r.Headers {
+		header.Set(k, v)
+	}
+
+	sv, err := slack.NewSecretsVerifier(header, v.secret)
+	if err != nil {
+		return err
+	}
+
+	sv.Write([]byte(r.Body))
+	return sv.Ensure()
+}
+
+func (v bearerVerifier) Verify(r events.LambdaFunctionURLRequest) error {
+	authHeader := r.Headers["authorization"]
+	if authHeader == "" {
+		return errors.New("Authorization header is missing")
+	}
+
+	headerParts := strings.Split(authHeader, " ")
+	if len(headerParts) != 2 || headerParts[0] != "Bearer" {
+		return errors.New("Invalid Authorization header format")
+	}
+
+	token := headerParts[1]
+	if token != v.secret {
+		return errors.New("Invalid token")
+	}
+
+	return nil
+}
+
+var config struct {
+	bootMode        string
+	requestVerifier requestVerifier
+}
+
+func init() {
+	config.bootMode = func() string {
+		m := os.Getenv("MODE")
+		if m == "" {
+			m = "dev"
+		}
+		return m
+	}()
+
+	secret := os.Getenv("AUTH_SECRET")
+	if secret == "" {
+		fmt.Println("Auth secret missing")
+		os.Exit(1)
+	}
+
+	switch config.bootMode {
+	case "dev":
+		config.requestVerifier = bearerVerifier{secret: secret}
+	case "prod":
+		config.requestVerifier = slackSignedSecretVerifier{secret: secret}
+	default:
+		fmt.Printf("Invalid boot mode: %s\n", config.bootMode)
+		os.Exit(1)
+	}
+}
+
 func main() {
 	debug := os.Getenv("DEBUG")
 
 	if debug != "" {
+		req := events.LambdaFunctionURLRequest{Headers: map[string]string{"authorization": "Bearer test-token"}}
+		if err := config.requestVerifier.Verify(req); err != nil {
+			fmt.Printf("Fail to verify request: %+v", err)
+			return
+		}
+
 		ctx := context.Background()
 
 		message, err := sendToOpenAI(ctx, "Hello!")
